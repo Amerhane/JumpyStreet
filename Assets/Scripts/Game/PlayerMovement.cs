fbf31a879bced2b62028c1cb5a23ab95f726ac72
jmglenewinkel <jmglenewinkel@my.waketech.edu>
2024-02-08 21:46:55 -0500

    added character movement using rigid body and fixed the camera so it doesn't rotate

--- Assets/Scripts/Game/PlayerMovement.cs
+++ Assets/Scripts/Game/PlayerMovement.cs
@@ -4,37 +4,61 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    [SerializeField] bool isJumping;
+    Vector3 nextDirection;
     Vector3 currentPosition;
-    Vector3 inputVector;
+
+    Rigidbody rb;
+    public float jumpForce;
+    public float moveSpeed;
+    public float rotationSpeed;
+
     public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        isJumping = false;
-        currentPosition = transform.position;
-        inputVector = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        rb = GetComponent<Rigidbody>();
+
+        jumpForce = 5f;
+        moveSpeed = 5f;
+        rotationSpeed = 1000f;
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(Input.GetKeyDown(KeyCode.W) && !isJumping)
-       {
-            inputVector += currentPosition;
-            Quaternion targetRotation = Quaternion.LookRotation(inputVector - currentPosition);
+        if (transform.position != new Vector3(currentPosition.x, transform.position.y, currentPosition.z) + nextDirection)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(currentPosition.x, transform.position.y, currentPosition.z) + nextDirection, moveSpeed * Time.deltaTime);
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.3f);
-            isJumping = true;
-            Jumping();
-       }
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(nextDirection), rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            nextDirection = Vector3.zero;
+            currentPosition = transform.position;
+            currentPosition.x = Mathf.Round(currentPosition.x);
+            currentPosition.y = Mathf.Round(currentPosition.y);
+
+            
+            if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                nextDirection.z = Input.GetAxisRaw("Horizontal");
+                Debug.Log(nextDirection);
+            }
+            else if (Input.GetAxisRaw("Vertical") != 0)
+            {
+                nextDirection.x = -Input.GetAxisRaw("Vertical");
+                Debug.Log("Vertical");
+            }
+        }
+
+
     }
 
-    public IEnumerator Jumping()
+    void Move()
     {
-        yield return new WaitForSeconds(0.75f);
-        isJumping = false;
+        rb.AddForce(0, jumpForce, 0);
     }
+
 }
