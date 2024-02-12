a7c30590e64f876e5c50f4b966ce2f65cf107c0e
Moose <61787005+the-fat-moose@users.noreply.github.com>
2024-02-12 18:22:20 -0500

    log spawn and move

---
+++ Assets/Scripts/Game/River.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class River : MonoBehaviour
+{
+    [Header("Water Prefabs")]
+    [SerializeField] private GameObject logPref;
+
+    private float randomSpawnRate = 0f;
+
+    private void Start()
+    {
+        randomSpawnRate = Random.Range(3f, 7f);
+
+        FlipOrientation();
+        InvokeRepeating("SpawnVehicle", 0f, randomSpawnRate);
+    }
+
+    private void SpawnVehicle()
+    {
+        float zSpawnCord = -7f;
+
+        if (transform.rotation.eulerAngles.y >= 170)
+        {
+            zSpawnCord *= -1f;
+        }
+
+        randomSpawnRate = Random.Range(5f, 7f);
+
+        GameObject spawnedObject = GameObject.Instantiate(logPref, new Vector3(transform.position.x, 0f, zSpawnCord), transform.rotation);
+
+        StartCoroutine(DespawnVehicle(spawnedObject));
+    }
+
+    private IEnumerator DespawnVehicle(GameObject vehicle)
+    {
+        yield return new WaitForSeconds(10f);
+
+        Destroy(vehicle);
+    }
+
+    // changes the orientation of the road
+    private void FlipOrientation()
+    {
+        int rand = Random.Range(0, 101);
+
+        if (rand >= 50)
+        {
+            transform.rotation = new Quaternion(0, 180, 0, 0);
+        }
+        else
+        {
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
+    }
+}
