013f5984691c6b92a5ae0588358cb8e166af6a0f
Moose <yugihanmaster321@gmail.com>
2024-02-23 23:28:51 -0500

    removed useless debug messages

--- Assets/Scripts/Game/ObstacleMovement.cs
+++ Assets/Scripts/Game/ObstacleMovement.cs
@@ -15,7 +15,6 @@ public class ObstacleMovement : MonoBehaviour
     void Start()
     {
         isCarFlipped = gameObject.transform.rotation.eulerAngles.y >= 170 ? true : false;
-        Debug.Log(isCarFlipped);
 
         obstacleRB = GetComponent<Rigidbody>();
         obstacleRB.freezeRotation = true;
@@ -24,15 +23,11 @@ public class ObstacleMovement : MonoBehaviour
 
     private void MoveForward()
     {
-        Debug.Log("BEING CALLED");
-
         int setZCord = 1;
 
         if (isCarFlipped)
         {
-            Debug.Log(true);
             setZCord *= -1;
-            Debug.Log("Z CORD " + setZCord);
         }
         if (obstacleRB != null)
         {
