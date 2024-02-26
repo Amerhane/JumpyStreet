9ef6b2789f303e17a453bd3fd37c5a00b5e7a490
Amerhane <jacobsafi@gmail.com>
2024-02-25 19:29:39 -0500

    Added Comment Header to DeathFallZone Script
    Added Comment Header to DeathFallZone Script

--- Assets/Scripts/Game/DeathFallZone.cs
+++ Assets/Scripts/Game/DeathFallZone.cs
@@ -1,7 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Project: JumpyStreet
+/// Name: Jacob Frigon
+/// Section: SGD 285.4171
+/// Instructor: Aurore Locklear
+/// Date: 02/25/2024
+/// </summary>
 public class DeathFallZone : MonoBehaviour
 {
     [SerializeField]
